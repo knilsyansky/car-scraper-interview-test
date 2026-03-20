fa9738b0c1c27766b7270be3270006514ee2c1c8
Roman <0knils0@gmail.com>
2026-03-20 05:22:05 +0500

    Тест запуска деплоя #2

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,35 @@
-console.log('Node js process started')
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        status: 'ok',
+        message: 'Backend is running!',
+        time: new Date().toISOString(),
+    });
+});
+
+app.get('/health', async (req, res) => {
+    try {
+        res.json({
+            db: 'connected', 
+            timestamp: new Date()
+        });
+    } catch (error) {
+        res.status(500).json({
+            db: 'error',
+            message: error.message,
+        });
+    }
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server started on ${PORT}`)
+});
